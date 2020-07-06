b204250d356a20093cf97fd81de1124ecbc2dcff
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-07-06 16:12:57 -0500

    deploy ready

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
-    "connectionString": (process.env.DB_URL === 'test') ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
+    "connectionString": (process.env.DATABASE_URL === 'test') ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
+    "ssl": !!process.env.SSL,
 }
--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
 	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || "development",
-	DB_URL:
-		process.env.DB_URL || "postgresql://anthonystolp@localhost/dd-uploads",
+	DATABASE_URL:
+		process.env.DATABASE_URL || "postgresql://anthonystolp@localhost/dd-uploads",
 	JWT_SECRET: process.env.JWT_SECRET || "you-are-the-secret",
+	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
 };
