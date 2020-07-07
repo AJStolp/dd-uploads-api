fdf7f67f31748b41c112f935238aaaba180fb660
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-07-07 15:16:46 -0500

    trying to deploy

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
 	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || "development",
 	DATABASE_URL: process.env.DATABASE_URL || "postgresql://anthonystolp@localhost/dd-uploads",
-	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
+	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://anthonystolp@localhost/dd-uploads-test",
 	JWT_SECRET: process.env.JWT_SECRET || "you-are-the-secret",
 };
