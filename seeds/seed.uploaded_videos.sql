13cbcff5485ca9f0f0fb38032981d87c1e745ac9
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-07-09 14:28:19 -0500

    gcs issue

--- seeds/seed.uploaded_videos.sql
+++ seeds/seed.uploaded_videos.sql
@@ -1,4 +1,4 @@
 INSERT INTO uploaded_video (title, content, video_url)
 VALUES
-    ('test video 1', 'content test 1', 'www.test.com'),
-    ('test video 2', 'content test 2', 'www.fake.com');
+    ('Roof Battles', 'Bummer Dude', 'https://storage.googleapis.com/anthonys-bucket/share-external%20(1).mp4');
+    
