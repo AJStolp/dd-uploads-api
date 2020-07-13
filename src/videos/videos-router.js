217668981f1fcb1c43bf6d9c5c09bea9e37640be
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-07-13 17:22:57 -0500

    gcs cred

--- src/videos/videos-router.js
+++ src/videos/videos-router.js
@@ -21,16 +21,15 @@ const myBucket = new GcsFileUpload(
 		keyFilename: serviceKey,
 		projectId: "rosy-sunspot-255301",
 		credentials: {
-			private_key: process.env.GOOGLE_APPLICATION_CREDENTIALS.private_key,
-			client_email: process.env.GOOGLE_APPLICATION_CREDENTIALS.client_email,
-			project_id: process.env.GOOGLE_APPLICATION_CREDENTIALS.project_id,
-			private_key_id: process.env.GOOGLE_APPLICATION_CREDENTIALS.private_key_id,
-			private_key: process.env.GOOGLE_APPLICATION_CREDENTIALS.private_key,
-			client_id: process.env.GOOGLE_APPLICATION_CREDENTIALS.client_id,
-			auth_uri: process.env.GOOGLE_APPLICATION_CREDENTIALS.auth_uri,
-			token_uri: process.env.GOOGLE_APPLICATION_CREDENTIALS.token_uri,
-			auth_provider_x509_cert_url: process.env.GOOGLE_APPLICATION_CREDENTIALS.auth_provider_x509_cert_url,
-			client_x509_cert_url: process.env.GOOGLE_APPLICATION_CREDENTIALS.client_x509_cert_url
+			project_id: process.env.project_id,
+			private_key_id: process.env.private_key_id,
+			private_key: process.env.private_key,
+			client_email: process.env.client_email,
+			client_id: process.env.client_id,
+			auth_uri: process.env.auth_uri,
+			token_uri: process.env.token_uri,
+			auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+			client_x509_cert_url: process.env.client_x509_cert_url
 		}
 	},
 	"anthonys-bucket"
