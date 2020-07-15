b04785950af26abee0973ff37532d9ab2f8e0211
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-07-15 12:11:34 -0500

    gcs

--- src/videos/videos-router.js
+++ src/videos/videos-router.js
@@ -22,7 +22,7 @@ const myBucket = new GcsFileUpload(
 		projectId: "rosy-sunspot-255301",
 		credentials: {
 			client_email: process.env.client_email,
-			private_key: process.env.private_key,
+			private_key: process.env.private_key.replace(/\\n/g, '\n'),
 		}
 	},
 	"anthonys-bucket"
