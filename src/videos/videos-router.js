4128356acc065ccc0ff4276259bf1bf392892a28
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-07-15 17:25:50 -0500

    gcs fix

--- src/videos/videos-router.js
+++ src/videos/videos-router.js
@@ -20,13 +20,13 @@ const myBucket = new GcsFileUpload(
 	{
 		keyFilename: serviceKey,
 		projectId: "rosy-sunspot-255301",
-		// credentials: {
-		// 	client_email: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
-		// 	private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY || "".replace(
-		// 		/\\n/g,
-		// 		"\n"
-		// 	 ),
-		// }
+		credentials: {
+			client_email: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+			private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY || "".replace(
+				/\\n/g,
+				"\n"
+			 ),
+		}
 	},
 	"anthonys-bucket"
 );
